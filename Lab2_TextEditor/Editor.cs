0e3c8416e318df588253df7571ce67cd17b1b18a
Chekushka <chekun.sergiy@gmail.com>
2021-11-09 20:33:28 +0200

    Project init

---
+++ Lab2_TextEditor/Editor.cs
@@ -0,0 +1,489 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace Lab2_TextEditor
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private int _tabCount = 0;
+
+        #region Methods
+
+        #region Tabs
+
+        private void AddTab()
+        {
+            var body = new RichTextBox();
+
+            body.Name = "Body";
+            body.Dock = DockStyle.Fill;
+            body.ContextMenuStrip = contextMenuStrip1;
+
+            var newPage = new TabPage();
+            _tabCount += 1;
+
+            var documentText = "Document " + _tabCount;
+            newPage.Name = documentText;
+            newPage.Text = documentText;
+            newPage.Controls.Add(body);
+
+            tabControl1.TabPages.Add(newPage);
+
+        }
+
+        private void RemoveTab()
+        {
+            if (tabControl1.TabPages.Count != 1)
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            }
+            else
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+                AddTab();
+            }
+        }
+
+        private void RemoveAllTabs()
+        {
+            foreach (TabPage page in tabControl1.TabPages)
+            {
+                tabControl1.TabPages.Remove(page);
+            }
+
+            AddTab();
+        }
+
+        private void RemoveAllTabsButThis()
+        {
+            foreach (TabPage page in tabControl1.TabPages)
+            {
+                if (page.Name != tabControl1.SelectedTab.Name)
+                {
+                    tabControl1.TabPages.Remove(page);
+                }
+            }
+        }
+
+        #endregion
+
+        #region SaveAndOpen
+
+        private void Save()
+        {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "RTF|.rtf";
+            saveFileDialog1.Title = "Save";
+
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+            }
+        }
+
+        private void SaveAs()
+        {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "Text Files|*.txt|VB Files|*.vb|C# Files|*.cs|Rtf Files|*.rtf|All Files|*.*";
+            saveFileDialog1.Title = "Save As";
+
+            if (saveFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            if (saveFileDialog1.FileName.Length > 0)
+            {
+                GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void Open()
+        {
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Filter = "RTF|*.rtf|Text Files|*.txt|VB Files|*.vb|C# Files|*.cs|All Files|*.*";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (openFileDialog1.FileName.Length > 9)
+                {
+                    GetCurrentDocument.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+            }
+
+        }
+
+        #endregion
+
+        #region TextFunctions
+
+        private void Undo()
+        {
+            GetCurrentDocument.Undo();
+        }
+
+        private void Redo()
+        {
+            GetCurrentDocument.Redo();
+        }
+
+        private void Cut()
+        {
+            GetCurrentDocument.Cut();
+        }
+
+        private void Copy()
+        {
+            GetCurrentDocument.Copy();
+        }
+
+        private void Paste()
+        {
+            GetCurrentDocument.Paste();
+        }
+
+        private void SelectAll()
+        {
+            GetCurrentDocument.SelectAll();
+        }
+
+        #endregion
+
+        #region General
+
+        private void GetFontCollection()
+        {
+            var insFonts = new InstalledFontCollection();
+
+            foreach (var item in insFonts.Families)
+            {
+                toolStripComboBox1.Items.Add(item.Name);
+            }
+
+            toolStripComboBox1.SelectedIndex = 0;
+        }
+
+        private void PopulateFontSizes()
+        {
+            for (int i = 1; i <= 75; i++)
+            {
+                toolStripComboBox2.Items.Add(i);
+            }
+
+            toolStripComboBox2.SelectedIndex = 11;
+        }
+
+        #endregion
+        
+        #endregion
+
+        #region Properties
+
+        private RichTextBox GetCurrentDocument => (RichTextBox)tabControl1.SelectedTab.Controls["Body"];
+
+        #endregion
+
+        #region EventBindings
+
+
+        private void AdvancedTextEditor_Load(object sender, EventArgs e)
+        {
+            AddTab();
+            GetFontCollection();
+            PopulateFontSizes();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetCurrentDocument.Text.Length > 0)
+            {
+                toolStripStatusLabel1.Text = GetCurrentDocument.Text.Length.ToString();
+            }
+        }
+
+        #region Menu
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddTab();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveTab();
+        }
+
+        #endregion
+
+        #region Toolbar
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            var boldFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Bold);
+            var regularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            if (GetCurrentDocument.SelectionFont.Bold)
+            {
+                GetCurrentDocument.SelectionFont = regularFont;
+            }
+            else
+            {
+                GetCurrentDocument.SelectionFont = boldFont;
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            var italicFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Italic);
+            var regularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            GetCurrentDocument.SelectionFont = GetCurrentDocument.SelectionFont.Italic ? regularFont : italicFont;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            var underlineFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Underline);
+            var regularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            GetCurrentDocument.SelectionFont = GetCurrentDocument.SelectionFont.Underline ? regularFont : underlineFont;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            var strikeout = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Strikeout);
+            var regularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            GetCurrentDocument.SelectionFont = GetCurrentDocument.SelectionFont.Strikeout ? regularFont : strikeout;
+        }
+
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectedText = GetCurrentDocument.SelectedText.ToUpper();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectedText = GetCurrentDocument.SelectedText.ToLower();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+
+            var newFontSize = GetCurrentDocument.SelectionFont.SizeInPoints + 2;
+
+            var newSize = new Font(GetCurrentDocument.SelectionFont.Name, newFontSize,
+                GetCurrentDocument.SelectionFont.Style);
+
+            GetCurrentDocument.SelectionFont = newSize;
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            var newFontSize = GetCurrentDocument.SelectionFont.SizeInPoints - 2;
+
+            var newSize = new Font(GetCurrentDocument.SelectionFont.Name, newFontSize,
+                GetCurrentDocument.SelectionFont.Style);
+
+            GetCurrentDocument.SelectionFont = newSize;
+        }
+
+        private void toolStripButton9_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                GetCurrentDocument.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void HighlightGreen_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor = Color.LightGreen;
+        }
+
+        private void HighlightOrange_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor = Color.Orange;
+        }
+
+        private void HighlightYellow_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor = Color.Yellow;
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var newFont = new Font(toolStripComboBox1.SelectedItem.ToString(), GetCurrentDocument.SelectionFont.Size,
+                GetCurrentDocument.SelectionFont.Style);
+
+            GetCurrentDocument.SelectionFont = newFont;
+        }
+
+        private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float.TryParse(toolStripComboBox2.SelectedItem.ToString(), out var newSize);
+
+            var newFont = new Font(GetCurrentDocument.SelectionFont.Name, newSize,
+                GetCurrentDocument.SelectionFont.Style);
+
+            GetCurrentDocument.SelectionFont = newFont;
+        }
+
+        #endregion
+
+        #region LeftToolStrip
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            AddTab();
+        }
+
+        private void RemoveTabToolStripButton_Click(object sender, EventArgs e)
+        {
+            RemoveTab();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        #endregion
+
+        #region ContextMenu
+
+        private void undoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+
+        private void redoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+
+        private void cutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void closeAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveAllTabs();
+        }
+
+        private void closeAllButThisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveAllTabsButThis();
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Lab2_TextEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab2_TextEditor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2_TextEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DC6D71B7-F12B-4E4E-B978-685C9142D7EB")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
